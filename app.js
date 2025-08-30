51240998609458ae45b85a1b63f58496f5147051
Thoai1503 <vothoai1503@gmail.com>
2025-08-30 17:50:11 +0700

    refix

--- app.js
+++ app.js
@@ -18,10 +18,7 @@ const path = require("path");
 
 app.use(
   cors({
-    origin: [
-      "https://electric-commercial.vercel.app",
-      "http://localhost:5173/",
-    ],
+    origin: ["https://electric-commercial.vercel.app", "http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true, // nếu cần cookie
