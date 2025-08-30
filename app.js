c3b20d71b28b95256f79de371c88ffbde5d219b9
Thoai1503 <vothoai1503@gmail.com>
2025-08-30 17:38:06 +0700

    change cors config url

--- app.js
+++ app.js
@@ -18,9 +18,13 @@ const path = require("path");
 
 app.use(
   cors({
-    origin: ["*"
+    origin: [
+      "https://electric-commercial.vercel.app",
+      "http://localhost:5000/",
     ],
-    credentials: true, // allow cookie/credentials entry
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true, // nếu cần cookie
   })
 );
 app.use(express.json());
