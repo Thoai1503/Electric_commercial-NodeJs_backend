974d3664ad5cc6859c0b3a04555c0e52a88e51c7
Thoai1503 <vothoai1503@gmail.com>
2025-09-01 20:00:02 +0700

    update category service

--- router/category.js
+++ router/category.js
@@ -3,8 +3,11 @@ var router = express.Router();
 const CategoryController = require("../controller/category");
 
 const categoryController = new CategoryController();
+const authMiddleware = require("../middleware/checkToken");
 
-router.route("/").post(categoryController.create);
+router
+  .route("/")
+  .post(authMiddleware.authenticateToken, categoryController.create);
 router.route("/:id").delete(categoryController.delete);
 
 module.exports = router;
